fff34c832e02dd1a384155d7abc32a56cdc3f30b
Samira Kaline <samirakaline123@gmail.com>
2022-03-14 22:54:54 -0300

    Criando conexao servidor/cliente

--- main.py
+++ main.py
@@ -1,29 +1,44 @@
+from ast import While
 from ticTacToe import TicTacToe
+import socket
 
+Host = 'localhost'
+Port = 50000
+
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.bind((Host,Port))
 ttt = TicTacToe()
 
 player = 0
 board = ttt.createBoard()
 winner = ttt.verifyWinner(board)
 
+while True:
+  print("Aguardando conexão com o cliente")
+  s.listen()
+  conn, addr = s.accept()
+  print('Conectado em: ', addr)
 
-while (not winner):
-  print("")
+  while (not winner):
+    print("")
+    ttt.printBoard(board)
+    print("")
+    line = ttt.getInputValid("Digite a linha: ")
+    column = ttt.getInputValid("Digite a coluna: ")
+
+    if (line == "Número não válido" or column == "Número não válido"):
+      break
+    
+    if (ttt.verifyMovement(board, line, column)):
+      ttt.makeMovement(board, line, column, player)
+      player = (player + 1) % 2
+    else:
+      print("A posição informada já está ocupada")
+    
+    winner = ttt.verifyWinner(board)
+
   ttt.printBoard(board)
-  print("")
-  line = ttt.getInputValid("Digite a linha: ")
-  column = ttt.getInputValid("Digite a coluna: ")
-
-  if (line == "Número não válido" or column == "Número não válido"):
-    break
+  print("Ganhador: ", winner)
+  conn.close()
+  break
   
-  if (ttt.verifyMovement(board, line, column)):
-    ttt.makeMovement(board, line, column, player)
-    player = (player + 1) % 2
-  else:
-    print("A posição informada já está ocupada")
-  
-  # winner = ttt.verifyWinner(board)
-
-print()
-# print("Ganhador: ", winner)
--- ticTacToe.py
+++ ticTacToe.py
@@ -27,7 +27,7 @@ class TicTacToe:
         return self.getInputValid(menssage)
     except:
       print("Número não válido")
-      return "Número não válido"
+      return self.getInputValid(menssage)
 
   def verifyMovement(self, board, line, column):
     if(board[line][column] == self.value):
@@ -39,4 +39,25 @@ class TicTacToe:
     board[line][column] = self.token[player]
 
   def verifyWinner(self, board):
-    return False
+    for i in range(3):
+      if(board[i][0] == board[i][1] and board[i][1] == board[i][2] and board[i][0] != self.value ):
+        return board[i][0]
+
+      if(board[0][i] == board[1][i] and board[1][i] == board[2][i] and board[0][i] != self.value ):
+        return board[0][i]
+
+    if(board[0][0]!= self.value and board[0][0] == board[1][1] and board[1][1] == board[2][2]):
+      return board[0][0]
+
+    if(board[0][2]!= self.value and board[0][2] == board[1][1] and board[1][1] == board[2][0]):
+      return board[0][2]
+
+    for line in board:
+      for column in line:
+        if (column == self.value):
+          return False
+
+    return "Empate"
+  
+  def randomPlayer(self,token):
+    pass
